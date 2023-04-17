de6b3ed5e7e28c8022343057f9e21bd562b33806
Vio Szabo <vio@vioszabo.se>
2023-04-17 15:28:11 +0200

    game done (codealong)

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Square } from 'components/Square'
 
 export const Board = () => {
-  // TODO - use the 'squares' state from the game reducer
-  const squares = []
+  const squares = useSelector((store) => store.game.squares)
 
   return (
     <div className="board">
       {squares.map((value, index) => (
+        // eslint-disable-next-line react/no-array-index-key
         <Square key={index} value={value} index={index} />
       ))}
     </div>
